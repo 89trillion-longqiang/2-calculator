37e9a00e0e3c1c3aab4bead25e795e805b598670
89trillion-longqiang <longqiang@89trillion.com>
2021-06-22 17:21:54 +0800

    ctrl:Increase parameter verification。handle:Increase the corresponding return parameters。router:Modify the ctrl package

--- handle/handle.go
+++ handle/handle.go
@@ -10,11 +10,24 @@ import (
 func HandleGetCalculator(uri string)map[string]string{
 	retMap := make(map[string]string,2)
 	newExp := strings.Replace(uri,"/C1/calculator?exp=","",1)///将获取的url切分，仅保留表达式部分
-	ret,err := service.Calculate(newExp)
-	retString := strconv.Itoa(ret)
+	if service.StringVer(newExp) == false{				///验证字符串是否合法
+		retMap["condition"] = "error"
+		retMap["result"]  = "StringVerification error"
+		return retMap
+	}
+
+	postfix := service.MixToPost(newExp)  ///转化为后缀表达式
+	if postfix == nil {
+		retMap["condition"] = "error"
+		retMap["result"]  = "StringVerification error"
+		return retMap
+	}
+
+	result ,err := service.GetResult(postfix)
+
 	if err == nil{
 		retMap["condition"] = "pass"
-		retMap["result"]= retString
+		retMap["result"]= strconv.Itoa(result)
 	}else {
 		retMap["condition"] = "error"
 		retMap["result"]  = err.Error()
