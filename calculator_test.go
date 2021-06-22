fd2cc5df3d8cca8b91ad3b8b2a8b6431a0790ceb
89trillion-longqiang <longqiang@89trillion.com>
2021-06-22 17:27:04 +0800

    test : add Test_StringVe Test_GetResult Test_MixToPost del Test_Calculate

--- calculator_test.go
+++ calculator_test.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-	"calculator/service"
 	"fmt"
 	"log"
 	"testing"
+
+	"calculator/service"
 )
 
-func Test_Calculate(t *testing.T) {
-
-	exp := "3+3+3"
-	ret, err := service.Calculate(exp)
-
-	if ret == 9 && err == nil {
-		fmt.Println("Test_Calculate pass")
-	} else {
-		log.Fatal("Test_Calculate error")
+func Test_StringVer(t *testing.T)  {
+	exp1 := "100++100-20*20"
+	exp2 := "100+100 -20*20"
+	if service.StringVer(exp1)==false && service.StringVer(exp2){
+		fmt.Println("Test_StringVer pass")
+	}else {
+		log.Fatal("Test_StringVer error")
 	}
+}
+
+
+func Test_GetResult(t *testing.T){
+	exp := "3*3+3*2-4"
+	prefix := service.MixToPost(exp)
+	result ,err := service.GetResult(prefix)
+	if result == 11  && err == nil{
+		fmt.Println("Test_GetResult pass")
+	} else if err != nil {
+		log.Fatal("Test_GetResult error")
+	}
+}
+
+func Test_MixToPost(t *testing.T){
+	postfix := []string{"3", "3", "*" ,"3", "2", "*", "+", "4", "-"}
+	exp := "3*3+3*2-4"
+	prefixRet := service.MixToPost(exp)
+	i1 := 0
+	i2 := 0
+	for ; i1 < len(postfix) && i2 < len(prefixRet) ; {
+		if postfix[i1] != prefixRet[i2] {
+			fmt.Println(i1, i2)
+			log.Fatal("Test_MixToPost error")
+		}
+		i1 ++
+		i2 ++
+	}
+	fmt.Println("Test_MixToPost pass")
 }
